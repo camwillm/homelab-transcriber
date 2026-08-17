a9cbb2ae02d59ef993bd6eec99f7e4e1bdc00b88
camwillm <camwillmofficial@gmail.com>
2026-08-16 22:27:53 -0400

    feat: save uploaded video files to disk with proper error handling

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+)
 
 func main() {
-	fmt.Println("Hello Transcriber")
+	http.HandleFunc("/transcribe", HandleTranscribe)
+
+	fmt.Println("Server starting on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func HandleTranscribe(w http.ResponseWriter, r *http.Request){
+	//here we only allow post request
+	if r.Method != "POST"{
+		fmt.Fprintf(w, "Use POST method\n")
+		return 
+	}
+	//parse the file
+	file, header, err := r.FormFile("video")
+	if err != nil{
+		fmt.Fprintf(w, "Error reading file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	//save thefile using os to the disk
+	out, err := os.Create(header.Filename)
+	if err != nil{
+		fmt.Fprintf(w, "Error saving file: %v\n", err)
+		return
+	}
+	defer out.Close()
+	//Copy the uploadedfile to the disk
+	_, err = io.Copy(out, file)
+	if err != nil{
+		fmt.Fprintf(w, "Error Writing File: %v\n", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Recieved and saved the file: %s\n", header.Filename)
 }
